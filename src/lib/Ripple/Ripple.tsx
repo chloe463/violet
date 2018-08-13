0a32117302b2b3200fba2047abb51327dfa52830
TsuyoshiHara <chloe463@gmail.com>
2018-08-14 01:23:48 +0900

    refactor(Ripple): Calculate coordinate in Ripple component class instead of Button component

--- src/lib/Ripple/Ripple.tsx
+++ src/lib/Ripple/Ripple.tsx
@@ -1,27 +1,61 @@
 import * as React from 'react';
+import { calculateRipplePosition, timer } from '../internals';
 
 import './Ripple.css';
 
 interface IRippleProps {
-  top: number;
-  left: number;
-  radius: number;
+  color?: string;
+}
+interface IRippleState {
+  ripples: any[];
 }
 
-export default class Ripple extends React.Component<IRippleProps> {
+export default class Ripple extends React.Component<IRippleProps, IRippleState> {
   constructor(props) {
     super(props);
+    this.state = { ripples: [] };
+    this.enableRippleEffect = this.enableRippleEffect.bind(this);
+  }
+
+  enableRippleEffect(event) {
+    const key = (new Date()).getMilliseconds() + Math.random();
+    const { top, left, radius } = calculateRipplePosition(event);
+
+    // Add ripple component
+    this.setState((prevState: IRippleState) => {
+      return {
+        ripples: [
+          ...prevState.ripples,
+          { key, top, left, radius }
+        ]
+      };
+    });
+
+    // Remove ripple component after 800ms.
+    timer(1300).then(() => {
+      this.setState((prevState: IRippleState) => {
+        return { ripples: prevState.ripples.filter(ripple => ripple.key !== key) };
+      });
+    });
   }
 
   render() {
-    const style = {
-      top:    this.props.top + 'px',
-      left:   this.props.left + 'px',
-      width:  this.props.radius + 'px',
-      height: this.props.radius + 'px'
-    };
     return (
-      <div className="Ripple" style={style}/>
+      <div className="Ripple" onMouseDown={this.enableRippleEffect}>
+        <div style={{position:'relative'}}>
+          {this.state.ripples.map(ripple => {
+            const style = {
+              position: 'absolute',
+              top: ripple.top + 'px',
+              left: ripple.left + 'px',
+              width: ripple.radius + 'px',
+              height: ripple.radius + 'px',
+              backgroundColor: this.props.color
+            } as React.CSSProperties;
+            return <div key={ripple.key} style={style} className="Ripple__effect"/>
+          })}
+        </div>
+      </div>
     );
   }
 }
